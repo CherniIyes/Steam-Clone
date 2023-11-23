5e9ce0192c2a3e535ef28601767b5be4fb8955f9
CheniIyes <cheniiyes@gmail.com>
2023-11-23 11:22:27 +0100

    linked_buttons_done

---
+++ log_in/login.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('.sign').on('click', function(event) {
+    window.location.href="http://127.0.0.1:5500/home_page/index2.html";
+
+  });
+});
+
+
+$(document).ready(function() {
+  $('.makeacc').on('click', function(event) {
+    window.location.href="http://127.0.0.1:5500/create%20account/create_account.html";
+
+  });
+});
